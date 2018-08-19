a2835f25be01e15abdba154d53da35f01607d41a
cyrus <online_mad@yahoo.com.hk>
2018-08-19 22:07:46 +0800

    fetch employee data

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,16 @@
 <template>
 <div id="dashboard">
-<h3>Dashboard</h3>
+<ul class="collection with-header">
+<li class="collection-header"><h4>Employees</h4></li>
+<li v-for="employee in employees" 
+v-bind:key="employee.id" class="collection-item">
+<div class="chip">{{employee.dept}}</div>
+{{employee.employee_id}}:{{employee.name}}
+<router-link class="secondary-content" :to="{name:'view-employee',params:{employee_id:employee.employee_id}}">
+<i class="fa fa-eye"></i>
+</router-link>
+</li>
+</ul>
 <div class="fixed-action-btn">
 <router-link to="/new" >
 <i class="fa fa-plus"></i>
@@ -21,13 +31,18 @@ export default{
         }
     },
     created(){
-        db.collection('employees').get().then
+        db.collection('employees').orderBy('dept').get().then
         (querySnapshot =>{
             querySnapshot.forEach(doc=>{
-                console.log(doc.data());
+               
                 const data = {
-
+                    'id':doc.id,
+                    'employee_id':doc.data().employee_id,
+                    'name':doc.data().name,
+                    'dept':doc.data().dept,
+                    'position':doc.data().position
                 }
+                this.employees.push(data)
             })
 
         })
