a3d3f24fd3396eb557ab6f19d51ff0d5213e715f
cyrus <online_mad@yahoo.com.hk>
2018-08-19 14:37:33 +0800

    first commit

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+<div id="dashboard">
+<h3>Dashboard</h3>
+<div class="fixed-action-btn">
+<router-link to="/new" >
+<i class="fa fa-plus"></i>
+</router-link>
+</div>
+</div>
+
+</template>
+
+<script>
+export default{
+    name: 'dashboard',
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default{
+    apiKey: "AIzaSyC2m5ur72lxbyLQ_nkZa9vUCx_saM6V_7A",
+    authDomain: "vuefirestore-425e2.firebaseapp.com",
+    databaseURL: "https://vuefirestore-425e2.firebaseio.com",
+    projectId: "vuefirestore-425e2",
+    storageBucket: "vuefirestore-425e2.appspot.com",
+    messagingSenderId: "864658888501"
+}
